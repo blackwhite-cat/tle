58add2934e545b27620ceecf66211db6e6742022
mkaminaga <mkaminaga2.718@gmail.com>
2017-10-11 15:31:24 +0900

    OUTDIR -> OBJDIR

--- tle_windows.mk
+++ tle_windows.mk
@@ -3,16 +3,16 @@
 # Copyright 2017 Mamoru Kaminaga
 CC = C:\"Program Files (x86)"\"Microsoft Visual Studio"\2017\Community\VC\Tools\MSVC\14.11.25503\bin\HostX86\x86\cl.exe
 LIB = C:\"Program Files (x86)"\"Microsoft Visual Studio"\2017\Community\VC\Tools\MSVC\14.11.25503\bin\HostX86\x86\lib.exe
-OUTDIR = build
+OBJDIR = build
 TARGET = tle.lib
 SRC = tle.cc
-OBJ = $(OUTDIR)/tle.obj
+OBJ = $(OBJDIR)/tle.obj
 CCFLAGS = /W4 /Zi /O2 /MT /EHsc /D"WIN32" /D"NODEBUG" /D"_LIB" /D"_UNICODE"\
-	/D"UNICODE" /D"NOMINMAX=0x0800" /Fo"$(OUTDIR)\\"
+	/D"UNICODE" /D"NOMINMAX=0x0800" /Fo"$(OBJDIR)\\"
 
 $(TARGET): $(OBJ)
 	$(LIB) /OUT:$(TARGET) $(OBJ)
 
-.cc{$(OUTDIR)}.obj:
+.cc{$(OBJDIR)}.obj:
 	@[ -d $(OBJDIR) ] || mkdir $(OBJDIR)
 	$(CC) $(CCFLAGS) /c $<
